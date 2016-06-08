a089fbc47b11ff81f7bbd3285f2d90ebc0918446
dylan cairns <invision33@gmail.com>
2016-06-07 22:56:05 -0500

    add firebase

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,19 +2,32 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     
+	headerMessage: 'Coming Soon',
+	responseMessage: '', 
+	emailAddress: '', 
+    userName: '',
+	
     isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/), 
-    isDisabled: Ember.computed.not("isValid"), 
-    
-    emailAddress: '', 
-    
-    userName: '', 
+    isDisabled: Ember.computed.not("isValid"),  
     
     actions: {
         saveInvitation() {
-            alert(`saving of the email ${this.get('emailAddress')} is in progress}`);
-            this.set('responseMessage', `Thanks! We've got your email address saved:
-                ${this.get('emailAddress')}`); 
-            this.set('emailAddress', '');
+            const email = this.get('emailAddress');
+			
+			const newInvitation = this.store.createRecord('invitation', {
+				email: email 
+			});
+			
+			newInvitation.save()
+				.then(response => {
+					this.set('responseMessage', `Thanks! We saved your email with the following id: 
+						${response.get('id')}`); 
+				})
+				.catch(e => {
+					console.log(`error: ${e.errrs}`); 
+				}) 
+											 
+			this.set('emailAddress', ''); 
 		}
     },
 });
